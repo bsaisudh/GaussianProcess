487c7be9fe5da2c1fc05ac6c8193dbbcea052d4a
bsaisudh <bsaisudh@terpmail.umd.edu>
2019-10-09 23:58:35 -0400

    added diffenret kernels to sin3x

--- sine3x_learn.py
+++ sine3x_learn.py
@@ -19,29 +19,41 @@ from sklearn.gaussian_process.kernels import ExpSineSquared as ESS
 xTrain, yTrain = readTrainFile("Data/problem4a_train.csv")
 xTest = readTestFile("Data/problem4a_test.csv")
 
-xTrain = np.atleast_2d(xTrain).T
-xTest = np.atleast_2d(xTest).T
+xTrain = np.atleast_2d(xTrain)
+xTest = np.atleast_2d(xTest)
 
-ess1 = ESS(1.0, 1.0, (1e-05, 100000.0), (1e-05, 100000.0))
-#kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
-#kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2)) + RBF(0.5, (-1e-2, 1e2))
-#kernel = C(1.0, (1e-3, 1e3)) * RQ(10, 0.5 ,(1e-2, 1e2)) + RBF(0.5, (1e-2, 1e2))
-kernel = C(1.0, (1e-3, 1e3)) * RQ(10, 0.5 ,(1e-2, 1e2)) * ess1 + RBF(0.5, (1e-2, 1e2))
-gp = GaussianProcessRegressor(kernel=kernel, alpha=0,#0.5 ** 5,
+C1 = C(1.0, (1e-3, 1e3))
+C2 = C(0.5, (1e-3, 1e3))
+RBF1 = RBF(10, (1e-2, 1e2))
+RBF2 = RBF(0.5, (1e-2, 1e2))
+RQ1 = RQ(10, 0.5 ,(1e-2, 1e2))
+ESS1 = ESS(1.0, 1.0, (1e-05, 100000.0), (1e-05, 100000.0))
+
+kernel1 = C1 * RBF1 + C2
+kernel2 = C1 * RBF1 + RBF2
+kernel3 = C1 * RQ1 + RBF2
+kernel4 = C1 * ESS1 + RBF2
+
+GP = []
+for kernel in [kernel1, kernel2, kernel3, kernel4]:
+    gp = GaussianProcessRegressor(kernel=kernel, #alpha=0.5 ** 5,
                               n_restarts_optimizer=10)
-gp.fit(xTrain, yTrain)
-y_pred, sigma = gp.predict(xTest, return_std=True)
-
-plt.figure()
-plt.plot(xTrain, yTrain, 'r:', label="sine(3x)")
-plt.errorbar(xTrain.ravel(), yTrain, 0, fmt='r.', markersize=10, label='Observations')
-plt.plot(xTest, y_pred, 'b-', label='Prediction')
-plt.fill(np.concatenate([xTest, xTest[::-1]]),
-         np.concatenate([y_pred - 1.9600 * sigma,
-                        (y_pred + 1.9600 * sigma)[::-1]]),
-         alpha=.5, fc='b', ec='None', label='95% confidence interval')
-plt.xlabel('x')
-plt.ylabel('sin(3x)')
-plt.legend(loc='upper left')
+    gp.fit(xTrain, yTrain)
+    GP.append(gp)
+    
+f, axs = plt.subplots(2,2)
+for ndx, gp, ax in zip([1,2,3,4], GP, [[0,0],[0,1],[1,0],[1,1]]):
+    y_pred, sigma = gp.predict(xTest, return_std=True)
+    axs[ax[0],ax[1]].plot(xTrain, yTrain, 'r:', label="sine(3x)")
+    axs[ax[0],ax[1]].errorbar(xTrain.ravel(), yTrain, 0, fmt='r.', markersize=10, label='Observations')
+    axs[ax[0],ax[1]].plot(xTest, y_pred, 'b-', label='Prediction')
+    axs[ax[0],ax[1]].fill(np.concatenate([xTest, xTest[::-1]]),
+             np.concatenate([y_pred - 1.95 * sigma,
+                            (y_pred + 1.95 * sigma)[::-1]]),
+             alpha=.5, fc='b', ec='None', label='95% confidence interval')
+    axs[ax[0],ax[1]].set_xlabel('x')
+    axs[ax[0],ax[1]].set_ylabel('sin(3x)')
+    axs[ax[0],ax[1]].legend(loc='upper left')
+    axs[ax[0],ax[1]].set_title(f"Kernel - {ndx}")
 
 plt.show()
