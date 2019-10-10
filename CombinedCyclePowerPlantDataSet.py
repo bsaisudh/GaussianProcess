55a3644022c655bba0fb269f735bae7145429c66
bsaisudh <bsaisudh@terpmail.umd.edu>
2019-10-09 22:17:27 -0400

    sin3x GP

--- CombinedCyclePowerPlantDataSet.py
+++ CombinedCyclePowerPlantDataSet.py
@@ -13,30 +13,24 @@ from matplotlib import pyplot as plt
 
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
-from sklearn.gaussian_process.kernels import RationalQuadratic as RQ
-from sklearn.gaussian_process.kernels import ExpSineSquared as ESS
 
-xTrain, yTrain = readTrainFile("Data/problem4a_train.csv")
-xTest = readTestFile("Data/problem4a_test.csv")
+xTrain, yTrain = readTrainFile("Data/problem4b_train.csv")
+xTest = readTestFile("Data/problem4b_test.csv")
 
 xTrain = np.atleast_2d(xTrain).T
 xTest = np.atleast_2d(xTest).T
 
-ess1 = ESS(1.0, 1.0, (1e-05, 100000.0), (1e-05, 100000.0))
-#kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
-#kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2)) + RBF(0.5, (-1e-2, 1e2))
-#kernel = C(1.0, (1e-3, 1e3)) * RQ(10, 0.5 ,(1e-2, 1e2)) + RBF(0.5, (1e-2, 1e2))
-kernel = C(1.0, (1e-3, 1e3)) * RQ(10, 0.5 ,(1e-2, 1e2)) * ess1 + RBF(0.5, (1e-2, 1e2))
-gp = GaussianProcessRegressor(kernel=kernel, alpha=0,#0.5 ** 5,
+kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+gp = GaussianProcessRegressor(kernel=kernel, alpha=0.5 ** 5,
                               n_restarts_optimizer=10)
 gp.fit(xTrain, yTrain)
 y_pred, sigma = gp.predict(xTest, return_std=True)
 
 plt.figure()
-plt.plot(xTrain, yTrain, 'r:', label="sine(3x)")
-plt.errorbar(xTrain.ravel(), yTrain, 0, fmt='r.', markersize=10, label='Observations')
+plt.plot(xTrain[0], yTrain, 'r:', label="sine(3x)")
+plt.errorbar(xTrain[0].ravel(), yTrain, 0, fmt='r.', markersize=10, label='Observations')
 plt.plot(xTest, y_pred, 'b-', label='Prediction')
-plt.fill(np.concatenate([xTest, xTest[::-1]]),
+plt.fill(np.concatenate([xTest[0], xTest[0][::-1]]),
          np.concatenate([y_pred - 1.9600 * sigma,
                         (y_pred + 1.9600 * sigma)[::-1]]),
          alpha=.5, fc='b', ec='None', label='95% confidence interval')
