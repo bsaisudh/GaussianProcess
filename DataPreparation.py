5f5c1011950996a0b30393cbb61be08e528b103f
bsaisudh <bsaisudh@terpmail.umd.edu>
2019-10-15 20:25:24 -0400

    execution procedure

--- DataPreparation.py
+++ DataPreparation.py
@@ -34,8 +34,22 @@ def readTestFile(fileNameTest, plot = False):
             plt.show()
         return X
 
+def readSolutionFile(fileNameTest, plot = False):
+    with open(fileNameTest) as file:
+        Y = []
+        rows = csv.reader(file, delimiter = ",")
+        for row in rows:
+            Y.append( [ float(i) for i in row ] )
+        if plot:
+            plt.figure()
+            plt.plot(Y,[0]*len(Y),'o')
+            plt.show()
+        return Y
+
 if __name__ == "__main__":
     fileNameTrain = "Data/problem4b_train.csv"
     fileNameTest = "Data/problem4b_test.csv"
+    fileNameSolution = "Data/problem4b_sol.csv"
     xTrain,yTrain = readTrainFile(fileNameTrain, True)
     xTest = readTestFile(fileNameTest, True)
+    yTest = readTestFile(fileNameSolution, True)
